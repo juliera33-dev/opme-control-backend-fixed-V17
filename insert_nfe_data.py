acb461ca5955d61f881a19eab0bf44649a65ccd6
juliera33-dev <julioteixeiraalves08@gmail.com>
2025-09-28 20:07:16 -0300

    Update insert_nfe_data.py

--- insert_nfe_data.py
+++ insert_nfe_data.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
-from models.opme import NotaFiscal, ItemNotaFiscal, db # Importação direta
+from models.opme import NotaFiscal, ItemNotaFiscal, db # Importação corrigida
 from flask import current_app
 from datetime import datetime
 
