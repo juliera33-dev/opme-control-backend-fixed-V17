3d1cf3a1e851c824802f93f59183d162fb0c5038
juliera33-dev <julioteixeiraalves08@gmail.com>
2025-09-28 23:09:29 -0300

    Corrigindo estrutura de rotas e modelos para o deploy

--- models/user.py
+++ models/user.py
@@ -18,20 +18,22 @@ class User(db.Model):
             'email': self.email
         }
 
-# --- MODELOS CORRIGIDOS E ADICIONADOS ABAIXO ---
+# --- MODELOS PADRONIZADOS E COMPLETOS ---
 
-# CLASSE RENOMEADA DE NFe PARA NotaFiscal
 class NotaFiscal(db.Model):
-    __tablename__ = 'nota_fiscal' # Trocado para nome mais padrão
+    __tablename__ = 'nota_fiscal'
     id = db.Column(db.Integer, primary_key=True)
     chave_acesso = db.Column(db.String(44), unique=True, nullable=False)
     numero = db.Column(db.String(9), nullable=False)
+    serie = db.Column(db.String(3))
     data_emissao = db.Column(DateTime, nullable=False)
+    destinatario_nome = db.Column(db.String)
+    destinatario_cnpj = db.Column(db.String)
+    cfop = db.Column(db.String)
+    tipo_operacao = db.Column(db.String)
     
-    # Relação com os itens
-    itens = db.relationship('ItemNotaFiscal', backref='nota_fiscal', lazy=True)
+    itens = db.relationship('ItemNotaFiscal', backref='nota_fiscal', lazy=True, cascade="all, delete-orphan")
 
-# NOVA CLASSE ADICIONADA
 class ItemNotaFiscal(db.Model):
     __tablename__ = 'item_nota_fiscal'
     id = db.Column(db.Integer, primary_key=True)
@@ -40,7 +42,6 @@ class ItemNotaFiscal(db.Model):
     quantidade = db.Column(db.Float, nullable=False)
     valor_total = db.Column(db.Float, nullable=False)
     
-    # Chave estrangeira para ligar o item à sua nota fiscal
     nota_fiscal_id = db.Column(db.Integer, db.ForeignKey('nota_fiscal.id'), nullable=False)
 
 class Produto(db.Model):
